639b5e9f22e7be564d9c99c3c376284db1f5620d
Almer Leo Almazan <almerleoalmazan@gmail.com>
2022-08-26 09:49:30 +0800

    Add customer configuration and password property in customer class

--- src/main/java/com/almeralmazan/springbootmasterclass/customer/CustomFakeRepository.java
+++ src/main/java/com/almeralmazan/springbootmasterclass/customer/CustomFakeRepository.java
@@ -1,6 +1,5 @@
 package com.almeralmazan.springbootmasterclass.customer;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,8 +9,8 @@ public class CustomFakeRepository implements CustomerRepo {
     @Override
     public List<Customer> getAllCustomers() {
         return List.of(
-                new Customer(1L, "Almer Leo Almazan"),
-                new Customer(2L, "Liezel Almazan")
+                new Customer(1L, "Almer Leo Almazan", "password123"),
+                new Customer(2L, "Liezel Almazan", "password123")
         );
     }
 }
--- src/main/java/com/almeralmazan/springbootmasterclass/customer/CustomerRepository.java
+++ src/main/java/com/almeralmazan/springbootmasterclass/customer/CustomerRepository.java
@@ -7,12 +7,11 @@ import java.util.Collections;
 import java.util.List;
 
 @Repository
-@Primary
 public class CustomerRepository implements CustomerRepo {
     @Override
     public List<Customer> getAllCustomers() {
         return Collections.singletonList(
-                new Customer(1L, "TODO: Implement real db")
+                new Customer(1L, "TODO: Implement real db", "password123")
         );
     }
 }
